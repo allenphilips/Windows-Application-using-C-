f7d20288744f09fb78c87fd3cf56de627f1dd3aa
allenphilips <98145104+allenphilips@users.noreply.github.com>
2022-03-18 10:39:08 +0530

    Add files via upload

---
+++ DigitToWord/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_num = new System.Windows.Forms.TextBox();
+            this.lbl_words = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 222);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt_num
+            // 
+            this.txt_num.Location = new System.Drawing.Point(295, 69);
+            this.txt_num.Name = "txt_num";
+            this.txt_num.Size = new System.Drawing.Size(100, 20);
+            this.txt_num.TabIndex = 1;
+            this.txt_num.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbl_words
+            // 
+            this.lbl_words.AutoSize = true;
+            this.lbl_words.Location = new System.Drawing.Point(311, 142);
+            this.lbl_words.Name = "lbl_words";
+            this.lbl_words.Size = new System.Drawing.Size(35, 13);
+            this.lbl_words.TabIndex = 2;
+            this.lbl_words.Text = "label1";
+            this.lbl_words.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_words);
+            this.Controls.Add(this.txt_num);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_num;
+        private System.Windows.Forms.Label lbl_words;
+    }
+}
+
